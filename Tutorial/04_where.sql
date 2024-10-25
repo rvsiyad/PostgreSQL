e13c0a2bb5a4936f8c7bf98e17b1160688e8a8c2
Roble <roblesiyad123@gmail.com>
2024-10-25 23:48:59 +0100

    Add 04_where

---
+++ Tutorial/04_where.sql
@@ -0,0 +1,8 @@
+SELECT *
+FROM "users"
+WHERE email IS NOT NULL;
+
+/*
+- Where clauses allows us to add conditions to out select statements.
+- Only select certain data if the condition is met.
+*/
