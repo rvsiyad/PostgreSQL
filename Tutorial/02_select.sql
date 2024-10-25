736256926a0faeeb2217b8d3f2a724fb3ca9a1be
Roble <roblesiyad123@gmail.com>
2024-10-25 23:48:34 +0100

    Add comments to 01_select

--- Tutorial/02_select.sql
+++ Tutorial/02_select.sql
@@ -1,3 +1,10 @@
 SELECT * FROM "users";
+-- ^^ This selects all columns in the "user" table. ^^
 
 SELECT id, email FROM "users";
+-- ^^ This selects the id and email columns in the "user" table. ^^
+
+/*
+- The SELECT command refers not in fact to the rows within the table, it refers to the columns!
+- We can add more columns to `SELECT` by separating them by commas.
+*/
