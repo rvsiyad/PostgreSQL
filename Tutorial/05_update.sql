ae5c838d57a3e24717f98ed5896d184316d3aa1f
Roble <roblesiyad123@gmail.com>
2024-10-25 23:49:10 +0100

    Add 05_update

---
+++ Tutorial/05_update.sql
@@ -0,0 +1,7 @@
+UPDATE "users" SET age = 30 WHERE name = 'jane doe';
+
+/*
+- Update statements will update specified parts of tables.
+- Must include the `set` keyword to ensure updates.
+- Where clauses are essential or the whole table will be updated to the set value.
+*/
