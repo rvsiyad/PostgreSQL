a09bbf00fa4d431a56352f5bdf299570bcd61304
Roble <roblesiyad123@gmail.com>
2024-10-25 23:49:19 +0100

    Add 06_delete

---
+++ Tutorial/06_delete.sql
@@ -0,0 +1,6 @@
+DELETE FROM "users" WHERE id = 1;
+
+/*
+- Delete statements will remove data from a specified table.
+- Important to include the where clause to specify on what condition to delete.
+*/
